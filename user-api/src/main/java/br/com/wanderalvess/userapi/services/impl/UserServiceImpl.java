39cac9c11f1fe166fba86159d7e75b494c7f22da
Wanderson Alves <wandersonalves.s@hotmail.com>
2022-07-07 20:57:01 -0300

    update load balancing

--- user-api/src/main/java/br/com/wanderalvess/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/wanderalvess/userapi/services/impl/UserServiceImpl.java
@@ -4,22 +4,28 @@ import br.com.wanderalvess.userapi.domain.User;
 import br.com.wanderalvess.userapi.repositories.UserRepository;
 import br.com.wanderalvess.userapi.services.UserService;
 import br.com.wanderalvess.userapi.services.exceptions.ObjectNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final Environment env;
     private final UserRepository repository;
 
     //Constructor
-    public UserServiceImpl(UserRepository repository) {
+    public UserServiceImpl(Environment env, UserRepository repository) {
+        this.env = env;
         this.repository = repository;
     }
 
     @Override
     public User findById(Long id) {
+        log.info("PAYROLL_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port" );
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
